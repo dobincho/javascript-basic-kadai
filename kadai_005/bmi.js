c6a2fef3bcfc25177c927c7226d3a957b2b2f5ff
dobincho <na096rie@icloud.com>
2024-08-01 20:17:37 +0900

    再提出します

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,8 +1,7 @@
 //変数の宣言・値の代入
 let userWeight = 68;
 let userHeight = 1.7;
-let userBmi = 'BMI'
+let userBmi = 68/(1.7*1.7)
 
 //コンソールへの出力
-console.log('BMI')
 console.log(userWeight/(userHeight*userHeight));
