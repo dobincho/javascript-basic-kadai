db34c39da2bb0adf422fe9d2c936c6afdc8c7879
dobincho <na096rie@icloud.com>
2024-08-04 20:05:45 +0900

    課題７提出

---
+++ kadai_007/calc.js
@@ -0,0 +1,25 @@
+let num =Math.floor(Math.random());
+console.log(num);
+
+
+num=15;
+
+//３と５の倍数
+if (num % 3 === 0 && num % 5 === 0) {
+  console.log('3と5の倍数です');
+}
+
+//３の倍数
+if (num % 3 === 0) {
+  console.log('3の倍数です');
+}
+
+//５の倍数
+if (num % 5 === 0) {
+  console.log('5の倍数です');
+}
+
+//いずれでもない場合
+else {
+  console.log(num);
+}
