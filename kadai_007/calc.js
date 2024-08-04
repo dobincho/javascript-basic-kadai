f34e38a6eca076240ba52636ad4c3aa909622300
dobincho <na096rie@icloud.com>
2024-08-04 20:29:39 +0900

    kadai_07再提出

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -9,15 +9,17 @@ if (num % 3 === 0 && num % 5 === 0) {
   console.log('3と5の倍数です');
 }
 
+
 //３の倍数
-if (num % 3 === 0) {
+else if (num % 3 === 0) {
   console.log('3の倍数です');
 }
 
 //５の倍数
-if (num % 5 === 0) {
+else if (num % 5 === 0) {
   console.log('5の倍数です');
 }
+
 
 //いずれでもない場合
 else {
