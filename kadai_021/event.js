6b73745d132411ec2790b3453e3a5d5b23749ec0
dobincho <na096rie@icloud.com>
2024-08-22 20:49:26 +0900

    kadai_021再提出

--- kadai_021/event.js
+++ kadai_021/event.js
@@ -3,14 +3,18 @@ const text = document.getElementById('text');
 //btnというidを持つHTML要素を取得し、定数に代入する
 const btn = document.getElementById('btn');
 
-
-
-
 //HTML要素がクリックされたときに非同期処理をする
 
-setTimeout(() => {
-  text.textContent='ボタンをクリックしました';
-}, 2000);
+btn.addEventListener('click',() => {
+  setTimeout(() => {
+    text.textContent='ボタンをクリックしました';
+  }, 2000);
+
+});
+
+
+
+
   
 
 
